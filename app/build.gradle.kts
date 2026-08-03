38148414d32a1b9c52c335df74c4c09e1ca6f240
Zennmn <176552149+Zennmn@users.noreply.github.com>
2026-08-03 19:47:19 +0800

    chore: bump version to 1.3.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "dev.amenhancer.module"
         minSdk = 26
         targetSdk = 37
-        versionCode = 89
-        versionName = "1.3.2"
+        versionCode = 90
+        versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
